9240aa37e43b5883b04eb471e199af485a3ad348
moiz shaikh <moeez4286@gmail.com>
2025-11-01 16:50:39 +0530

    static keyword

---
+++ staticKeyword.java
@@ -0,0 +1,20 @@
+public class staticKeyword {
+    public static void main(String[] args) {
+        
+    }
+}
+
+class Student{
+
+    String name;
+    static String schoolName;
+
+    void setName(String name)
+    {
+this.name=name;
+    }
+String getName(){
+        return name;
+
+    }
+}
